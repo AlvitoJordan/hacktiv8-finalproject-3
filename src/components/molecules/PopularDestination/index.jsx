11dbe65899a8d76765c475f9ce3bb0ba21d431d1
Syaeful Annas <syaefulannas33@gmail.com>
2023-11-22 10:19:35 +0700

    refactor top destination

--- src/components/molecules/PopularDestination/index.jsx
+++ src/components/molecules/PopularDestination/index.jsx
@@ -1,19 +1,61 @@
-import { Image, ScrollView, StyleSheet, TouchableOpacity, View } from "react-native";
+import { Image, ImageBackground, ScrollView, StyleSheet, TouchableOpacity, View } from "react-native";
 import React from "react";
 import { Gap, TextCS } from "../../atoms";
 import { colors } from "../../../utils/colors";
-import { DUHotelIbis } from "../../../assets";
+import { DUHotelIbis, DUHotelManila, ICFavorite, ICRate } from "../../../assets";
+import CardHotel from "../Card/CardHotel";
+import { Colors } from "react-native/Libraries/NewAppScreen";
 
 const PopularDestination = () => {
   return (
     <View style={styles.container}>
       <TextCS style={styles.title}>Popular Destination Indonesia</TextCS>
       <Gap height={20} />
-      <ScrollView horizontal style={styles.listDestination}>
-        <TouchableOpacity style={styles.imageWrapper}>
-          <Image source={DUHotelIbis} style={styles.image} />
-          <TextCS style={styles.location}>Hotel Ibis</TextCS>
-        </TouchableOpacity>
+      <ScrollView horizontal showsHorizontalScrollIndicator={false} style={styles.listDestination}>
+        <View style={styles.containerCard}>
+          <ImageBackground source={DUHotelManila} style={styles.image} imageStyle={{ borderTopLeftRadius: 20, borderTopRightRadius: 20 }}>
+            <View style={styles.imageWrapper}>
+              <TouchableOpacity style={styles.favoriteIcon}>
+                <ICFavorite />
+              </TouchableOpacity>
+            </View>
+          </ImageBackground>
+          <TouchableOpacity onPress={""} style={styles.detailsContainer}>
+            <View>
+              <TextCS style={styles.titleHotel}>Kingfords Hotel Manila</TextCS>
+              <Gap height={5} />
+              <ICRate />
+              <Gap height={10} />
+              <TextCS>Filipina</TextCS>
+            </View>
+            <View>
+              <TextCS style={styles.price}>$ 500</TextCS>
+              <TextCS style={styles.day}>Per day</TextCS>
+            </View>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.containerCard}>
+          <ImageBackground source={DUHotelIbis} style={styles.image} imageStyle={{ borderTopLeftRadius: 20, borderTopRightRadius: 20 }}>
+            <View style={styles.imageWrapper}>
+              <TouchableOpacity style={styles.favoriteIcon}>
+                <ICFavorite />
+              </TouchableOpacity>
+            </View>
+          </ImageBackground>
+          <TouchableOpacity onPress={""} style={styles.detailsContainer}>
+            <View>
+              <TextCS style={styles.titleHotel}>Kingfords Hotel Manila</TextCS>
+              <Gap height={5} />
+              <ICRate />
+              <Gap height={10} />
+              <TextCS>Filipina</TextCS>
+            </View>
+            <View>
+              <TextCS style={styles.price}>$ 500</TextCS>
+              <TextCS style={styles.day}>Per day</TextCS>
+            </View>
+          </TouchableOpacity>
+        </View>
       </ScrollView>
     </View>
   );
@@ -24,6 +66,7 @@ export default PopularDestination;
 const styles = StyleSheet.create({
   container: {
     width: "100%",
+    paddingBottom:20
   },
   title: {
     fontWeight: "600",
@@ -33,12 +76,9 @@ const styles = StyleSheet.create({
   imageWrapper: {
     height: 240,
     backgroundColor: colors.white,
-    shadowColor: "#d3d3d3",
-    shadowOffset: { width: -2, height: 4 },
-    shadowOpacity: 0.2,
-    shadowRadius: 3,
-    elevation: 5,
     borderRadius: 20,
+    borderColor: colors.gray,
+    borderWidth: 3,
   },
   image: {
     width: 200,
@@ -52,5 +92,50 @@ const styles = StyleSheet.create({
     fontWeight: "600",
     fontSize: 24,
     padding: 10,
+  },
+  containerCard: {
+    width: 220,
+    borderRadius: 20,
+    backgroundColor: colors.white,
+    height: 300,
+    marginRight:10,
+    elevation: 1,
+    borderWidth:2,
+    borderColor: colors.gray
+  },
+  image: {
+    height: 180,
+    padding: 10,
+  },
+  imageWrapper: {
+    width: "100%",
+    justifyContent: "flex-end",
+    flexDirection: "row",
+  },
+  favoriteIcon: {
+    width: 35,
+    height: 35,
+    backgroundColor: colors.white,
+    borderRadius: 100,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  detailsContainer: {
+    padding: 20,
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  titleHotel: {
+    fontWeight: "700",
+    fontSize: 15,
+    width: 100
+  },
+  price: {
+    color: colors.primary,
+    fontWeight: "700",
+    fontSize: 15,
+  },
+  day: {
+    color: colors.primary,
   },
 });
